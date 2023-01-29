c02b0aeb8790aed2610c51a7ab427dd727c1f151
Lea23VC <leandro.villalobos.dx@gmail.com>
2023-01-29 18:18:30 -0300

    chore: remove unused context files

--- src/context/queryInfoContext.tsx
+++
@@ -1,8 +0,0 @@
-import { Dispatch, SetStateAction, createContext } from "react";
-
-type ContextProps = {
-  activeTab: number;
-  setActiveTab: Dispatch<SetStateAction<number>>;
-};
-
-export const QueryInfoContext = createContext({} as ContextProps);
